b7af80fe7fd5c79d819916cb3ef16ddfd592119f
Sam Pool <sambpool@gmail.com>
2021-12-20 13:59:27 -0700

    add getByUser

--- src/server/controllers/list.ts
+++ src/server/controllers/list.ts
@@ -1,7 +1,16 @@
 import {Request, Response} from "express";
+import {Document} from "mongoose";
+
+const model = require('../models/list');
 
 module.exports = {
+    getByUser: function getListsByUser(req: Request, res: Response){
+        console.log(req.params);
+        model.findByUser(req.params.user)
+            .then((data: Document[]) => res.send(data));
+    },
     addList: function addList(req: Request, res: Response){
-        console.log('add list');
+        model.create(req.body.user, req.body.name)
+            .then((data: Document) => res.send(data));
     }
 };

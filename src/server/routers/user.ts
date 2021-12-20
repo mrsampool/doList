5d77529f3ccb2f94917ffc7d50fc9980f1de3fd7
Sam Pool <sambpool@gmail.com>
2021-12-20 14:00:54 -0700

    add logging

--- src/server/routers/user.ts
+++ src/server/routers/user.ts
@@ -1,9 +1,15 @@
-import {Router} from "express";
+import {NextFunction, Request, Response, Router} from "express";
 const listRouter = require('./list');
 const userRouter = Router();
 
+process.env.NODE_ENV !== 'production' && userRouter.use((req: Request, res: Response, next: NextFunction) =>{
+    console.log('User Router');
+    console.log(req.path);
+    next();
+});
+
 // TODO: userRouter.post('/');
 // TODO: userRouter.get('/:userId');
-userRouter.use('/:userId/list', listRouter);
+userRouter.use('/:user/list', listRouter);
 
 module.exports = userRouter;

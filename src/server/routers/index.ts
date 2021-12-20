0d65b016097c4ea622f5ce8ea44547cba5a3ff18
Sam Pool <sambpool@gmail.com>
2021-12-20 14:02:21 -0700

    add logging

--- src/server/routers/index.ts
+++ src/server/routers/index.ts
@@ -1,6 +1,12 @@
-import {Router} from 'express';
+import {NextFunction, Request, Response, Router} from 'express';
 const apiRouter = Router();
 const userRouter = require('./user');
+
+process.env.NODE_ENV !== 'production' && apiRouter.use((req: Request, res: Response, next: NextFunction) =>{
+    console.log('API Router');
+    console.log(req.path);
+    next();
+});
 
 apiRouter.use('/user', userRouter);
 

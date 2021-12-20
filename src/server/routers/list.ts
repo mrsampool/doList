896b548cdb139a7990905cbfc82bf6a0738f0549
Sam Pool <sambpool@gmail.com>
2021-12-20 14:01:49 -0700

    add groupRouter

--- src/server/routers/list.ts
+++ src/server/routers/list.ts
@@ -1,9 +1,17 @@
-import {Router} from "express";
+import {NextFunction, Request, Response, Router} from "express";
 const controller = require('../controllers');
-const listRouter = Router();
+const groupRouter = require('./group');
+const listRouter = Router({mergeParams: true});
 
-// TODO: listRouter.get('/')
+process.env.NODE_ENV !== 'production' && listRouter.use((req: Request, res: Response, next: NextFunction) =>{
+    console.log('List Router');
+    console.log(req.path);
+    next();
+});
+
+listRouter.get('/', controller.list.getByUser);
 listRouter.post('/', controller.list.addList);
+listRouter.use('/:listId/group', groupRouter);
 
 // TODO: listRouter.get('/:listId')
 // TODO: listRouter.put('/:listId')

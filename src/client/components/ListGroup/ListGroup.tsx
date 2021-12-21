185595961f764aecbc856c836d10ed03942c1e41
sampool <sambpool@gmail.com>
2021-12-21 14:35:37 -0700

    add ListGroup item map

--- src/client/components/ListGroup/ListGroup.tsx
+++ src/client/components/ListGroup/ListGroup.tsx
@@ -4,10 +4,21 @@ import React from 'react';
 // Stylesheet
 import './ListGroup.css';
 import {GroupInterface} from "../../../lib/interfaces/GroupInterface";
+import {ItemInterface} from "../../../lib/interfaces/ItemInterface";
 
-const ListGroup = (props:ListGroupProps) => (
-  <li></li>
-);
+const ListGroup = ({ group }:ListGroupProps) => {
+    const { name, items } = group;
+    return (
+        <li>
+            {name}
+            <ul>
+                { items.map((item: ItemInterface) => {
+                    return <li key={`${name}Group-${item.name}Item`}>{item.name}</li>
+                })}
+            </ul>
+        </li>
+    );
+};
 export default ListGroup;
 
 interface ListGroupProps {

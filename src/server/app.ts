3b97da1397f7e9f47b04aaca85882ab2557c4eca
sampool <sambpool@gmail.com>
2021-12-21 13:52:41 -0700

    serve static files

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,8 +1,10 @@
 // Libraries / Modules
 import * as express from "express";
+const path = require('path');
 const apiRouter = require('./routers');
 
 const app = express();
+app.use(express.static(path.join(__dirname, '..', 'client')));
 app.use(express.json());
 
 process.env.NODE_ENV !== 'production' && app.use((req, res, next) => {

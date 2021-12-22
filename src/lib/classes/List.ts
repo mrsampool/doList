25faf49eb567f5f8c55bce3f8033280a7079f447
sampool <sambpool@gmail.com>
2021-12-22 10:57:54 -0700

    add _id property

--- src/lib/classes/List.ts
+++ src/lib/classes/List.ts
@@ -1,11 +1,18 @@
 import {GroupInterface} from "../interfaces/GroupInterface";
+import {ObjectId} from "mongoose";
 
 export class List {
+  _id?: ObjectId | String;
   name: String;
+  user: String;
   groups: GroupInterface[]|[];
 
-  constructor(name?: String) {
-    this.name = name || '';
-    this.groups = []
+  constructor(name?: String, id?: String) {
+    this.name = name || ''
+    this.groups = [];
+    this.user = '';
+    if (id) {
+      this._id = id;
+    }
   }
 }

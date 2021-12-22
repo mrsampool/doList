e3c4eab7ccf74d652bde2a8e531966e4fce393e0
sampool <sambpool@gmail.com>
2021-12-22 11:41:39 -0700

    remove initial values

--- src/client/AppContext.tsx
+++ src/client/AppContext.tsx
@@ -1,8 +1,3 @@
 import {createContext} from "react";
-import {List} from "../lib/classes/List";
 
-export const AppContext = createContext({
-    user: {},
-    userLists: [new List()],
-    currentList: new List(),
-});
+export const AppContext = createContext({});

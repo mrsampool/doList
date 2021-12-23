6edb4edb42db816423455a729994d05b9c207e34
sampool <sambpool@gmail.com>
2021-12-22 17:24:13 -0700

    start list item UI

--- src/client/components/GroupItem/GroupItem.tsx
+++ src/client/components/GroupItem/GroupItem.tsx
@@ -1,21 +1,43 @@
-import {ReactNode}from "react";
+import {ReactNode, useState}from "react";
 import {ItemInterface} from "../../../lib/interfaces/ItemInterface";
 
 // Stylesheet
 import './GroupItem.css';
 
-const GroupItem = ({item}:ListGroupItemProps) => (
-    <li>
-        <label>
+const GroupItem = ({item}:ListGroupItemProps) => {
+    const [editMode, setEditMode] = useState(false);
+    function toggleEditMode(){
+        setEditMode(!editMode);
+    }
+    function handleSubmit(){
+        setEditMode(false);
+    }
+    return (
+        <li className="list-item">
+            <label>
             <input type="checkbox" defaultChecked={item.status}/>
-            {item.name}
-            <button> {`>`} </button>
-        </label>
-    </li>
-);
+                {
+                    !editMode
+                        ? (
+                            <span className="item-name">
+                                {item.name}
+                                <button onClick={toggleEditMode}>edit</button>
+                            </span>
+                        )
+                        : (
+                            <form onSubmit={handleSubmit} onBlur={handleSubmit}>
+                                <input defaultValue={item.name} />
+                                <button type="submit">done</button>
+                            </form>
+                        )
+                }
+            </label>
+        </li>
+    )
+}
 export default GroupItem;
 
 interface ListGroupItemProps {
-    children: ReactNode
+    children: ReactNode,
     item: ItemInterface
 }

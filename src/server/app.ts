95070d58565b77c96f50da242187882287721383
sampool <sambpool@gmail.com>
2021-12-23 05:34:37 -0700

    start passport

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,10 +1,26 @@
 // Libraries / Modules
 import * as express from "express";
+const passport = require('passport');
 const path = require('path');
 const apiRouter = require('./routers');
 
 const app = express();
 app.use(express.static(path.join(__dirname, '..', 'client')));
+require('./authenticate')();
+require('dotenv').config();
+
+/*
+app.use(
+    session({
+        store: new PgSession({ pool }),
+        secret: 'my secret',
+        resave: false,
+        cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 },
+    }),
+);
+*/
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.json());
 
 process.env.NODE_ENV !== 'production' && app.use((req, res, next) => {

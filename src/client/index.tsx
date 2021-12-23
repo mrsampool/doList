21f37250be70f6758c1747b3f46d7bc0e0c04c3e
sampool <sambpool@gmail.com>
2021-12-23 05:32:43 -0700

    add global styles

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
 import {render} from "react-dom";
+import './globalStyles/reset.css';
+import './globalStyles/colors.css';
+import './globalStyles/text.css';
+import './globalStyles/elements.css';
 import App from "./App";
 
 render(<App />, document.getElementById('root'));

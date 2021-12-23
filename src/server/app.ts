a0ad68470ad2198633da32aa330114f0f78e2c08
sampool <sambpool@gmail.com>
2021-12-23 05:52:44 -0700

    implement passport sessions

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,24 +1,30 @@
 // Libraries / Modules
 import * as express from "express";
 const passport = require('passport');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 const path = require('path');
 const apiRouter = require('./routers');
 
 const app = express();
 app.use(express.static(path.join(__dirname, '..', 'client')));
-require('./authenticate')();
-require('dotenv').config();
+//require('./authenticate')();
+//require('dotenv').config();
 
-/*
+const store = new MongoDBStore({
+    uri: process.env.DATABASE_URL || 'mongodb://localhost/doList',
+    collection: 'sessions'
+});
+store.on('error', (err: Error) => console.log(err));
 app.use(
     session({
-        store: new PgSession({ pool }),
-        secret: 'my secret',
-        resave: false,
         cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 },
+        secret: 'my secret',
+        store: store,
+        resave: true,
+        saveUninitialized: true
     }),
 );
-*/
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.json());
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,6 +1,6 @@
 import * as mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost/doList')
+mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost/doList')
     .then(() => 'connected to MongoDB')
     .catch((err) => console.log(err));
 
